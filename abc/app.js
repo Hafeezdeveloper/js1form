3eeb1434adb95f9e1aa9269f6e1bae4bab4d1616
Hafeezdeveloper <Hafeez.hussain2003@gmail.com>
2022-10-01 12:42:17 +0500

    java1form

---
+++ abc/app.js
@@ -0,0 +1,15 @@
+let a = document.getElementById('bx1')
+let b = document.getElementById('bx2')
+
+
+
+function abc(){
+    bx1.style.display = 'block'
+    bx2.style.display = 'none'
+
+}
+function xyz(){
+    bx1.style.display = 'none'
+    bx2.style.display = 'block'
+
+}
